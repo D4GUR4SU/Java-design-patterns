1fa109cc920f678260bfb2bd42b1f42b23d1f95c
Dagurasu56 <douglas_search@hotmail.com>
2022-01-24 19:07:38 -0300

    creating example of adapter

---
+++ corepatterns/src/com/dagurasu/patterns/adapter/WeatherAdapter.java
@@ -0,0 +1,16 @@
+package com.dagurasu.patterns.adapter;
+
+public class WeatherAdapter {
+
+	public int findTemperature(int zipCode) {
+		String city = null;
+
+		if (zipCode == 19406) {
+			city = "Narnia";
+		}
+
+		WeatherFinder finder = new WeatherFinderImpl();
+		int temperature = finder.find(city);
+		return temperature;
+	}
+}
---
+++ corepatterns/src/com/dagurasu/patterns/adapter/WeatherUI.java
@@ -0,0 +1,14 @@
+package com.dagurasu.patterns.adapter;
+
+public class WeatherUI {
+
+	public void showTemperature(int zipCode) {
+		WeatherAdapter adapter = new WeatherAdapter();
+		System.out.println(adapter.findTemperature(19406));
+	}
+	
+	public static void main(String[] args) {
+		WeatherUI ui = new WeatherUI();
+		ui.showTemperature(19406);
+	}
+}
