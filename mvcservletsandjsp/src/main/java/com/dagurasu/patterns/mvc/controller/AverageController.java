3a05ce60152f2f814e817d0ce670ba7b2053f7fa
Dagurasu56 <douglas_search@hotmail.com>
2022-01-25 11:45:35 -0300

    creating a project with example of mvc and servlets jsp

---
+++ mvcservletsandjsp/src/main/java/com/dagurasu/patterns/mvc/controller/AverageController.java
@@ -0,0 +1,32 @@
+package com.dagurasu.patterns.mvc.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dagurasu.patterns.mvc.model.AverageModel;
+
+@WebServlet("/averageController")
+public class AverageController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		int num1 = Integer.parseInt(request.getParameter("number1"));
+		int num2 = Integer.parseInt(request.getParameter("number2"));
+
+		AverageModel model = new AverageModel();
+		model.calculateAverage(num1, num2);
+		int result = model.calculateAverage(num1, num2);
+		request.setAttribute("result", result);
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("result.jsp");
+		dispatcher.forward(request, response);
+	}
+}
---
+++ mvcservletsandjsp/src/main/java/com/dagurasu/patterns/mvc/model/AverageModel.java
@@ -0,0 +1,8 @@
+package com.dagurasu.patterns.mvc.model;
+
+public class AverageModel {
+
+	public int calculateAverage(int number1, int number2) {
+		return (number1 + number2) / 2;
+	}
+}
