0ad4a76e521b112c4e3158cd6319d65152cfcea7
Dagurasu56 <douglas_search@hotmail.com>
2022-01-25 12:32:26 -0300

    creating a project with example of mvc using spring boot

---
+++ mvc-springboot/src/main/java/com/dagurasu/patterns/mvc/controllers/HelloController.java
@@ -0,0 +1,15 @@
+package com.dagurasu.patterns.mvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String hello(ModelMap modelMap) {
+		modelMap.addAttribute("msg", "Douglas");
+		return "helloMvc";
+	}
+}
